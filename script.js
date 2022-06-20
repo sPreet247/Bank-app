5da6037382619254993849e03aeafa92282f4685
sPreet247 <samre2472@gmail.com>
2022-06-19 21:09:37 -0400

    sorted-button-logic

--- script.js
+++ script.js
@@ -216,6 +216,13 @@ btnClose.addEventListener('click', function (e) {
 
   inputCloseUsername.value = inputClosePin.value = '';
 });
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
